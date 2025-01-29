eae336bde1a49b2ba92bcd3aa9b6d50541d489b7
Harsh Rai <htc38199@gmail.com>
2025-01-29 19:23:01 +0530

    fix/made the upgrader for ws

--- server/cmd/server.go
+++ server/cmd/server.go
@@ -3,17 +3,29 @@ package main
 import (
 	"drizlink/server/interfaces"
 	connection "drizlink/server/internal"
+	"net/http"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
+	//Intialize websocket upgrader
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
 	server := interfaces.Server{
-		Address:     "localhost:8080",
+		Address:     ":8080",
 		Connections: make(map[string]*interfaces.User),
 		IpAddresses: make(map[string]*interfaces.User),
 		Messages:    make(chan interfaces.Message),
+		Upgrader:    upgrader,
 	}
 
-	go connection.StartHeartBeat(100*time.Second, &server)
+	go connection.StartHeartBeat(30*time.Second, &server)
 	connection.Start(&server)
 }
